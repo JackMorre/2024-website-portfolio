fb1bc94a9f917e3dee0a7f7c7566428aa1aed373
Jack <jack.morrell79@googlemail.com>
2024-03-11 19:56:38 +0000

    changing socials in menu - mobile

--- src/pages/componants/aside/Aside.jsx
+++ src/pages/componants/aside/Aside.jsx
@@ -13,7 +13,7 @@ export default function Aside() {
             desktopMode
               ? "w-[50px] xl:w-[70px] fixed z-30  border-r-[1px] border-dark top-0 left-0"
               : "fixed top-0 right-0 w-5/6 shadow-[-0px_0px_50px_10px_#0F0E0E] pt-16 z-40 bg-light"
-          } h-screen flex flex-col justify-between bg-light`}
+          } h-screen flex flex-col bg-light`}
         >
           <ul
             className={`font-merriweather ${
